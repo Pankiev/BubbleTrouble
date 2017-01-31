fc7a6aec794f8d61a953fc015ba949bb71378386
Adrian Pankiewicz <farami11@tlen.pl>
2017-01-31 20:59:22 +0100

    Init

---
+++ desktop/src/com/bubbletrouble/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bubbletrouble.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bubbletrouble.game.BubbleTrouble;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BubbleTrouble(), config);
+	}
+}
